ebdeec7ce9a53c4394e0d7985c923fe4c4c68942
doroz <125635258+devdoroz@users.noreply.github.com>
2024-06-03 23:18:35 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,26 @@
+local debug = {}
+
+local player = game:GetService("Players")
+local coreGui = game:GetService("CoreGui")
+
+local ui = game:GetObjects("rbxassetid://17728321239")[1]
+local debugUI = ui.debug:Clone()
+
+debugUI.Parent = coreGui
+
+function debug:newValue(identifier, value)
+	local textLabel = ui.TextLabel:Clone()
+	textLabel.Parent = debugUI.Frame
+	textLabel.Name = identifier
+	textLabel.Text = identifier..": "..value
+end
+
+function debug:updateValue(identifier, value)
+	debugUI.Frame[identifier].Text = identifier..": "..value
+end
+
+function debug:Destroy()
+	ui:Destroy()
+end
+
+return debujg
